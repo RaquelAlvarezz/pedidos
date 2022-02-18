ab5297e24a318453bc9bb6a4269f4c02a466fde8
RaquelAlvarezz <91055857+RaquelAlvarezz@users.noreply.github.com>
2022-02-18 10:37:43 +0100

    ghangues

---
+++ .history/Ficheros/categorias_20220218092651.php
@@ -0,0 +1,42 @@
+<?php
+//comprueba que el usuario haya abierto sesión o redirige
+require 'sesiones.php';
+require_once 'bd.php';
+comprobar_sesion();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> 
+    <link href="./node_modules/bootstrap/dist/css/styles.css">
+    <script src="../src/components/header.js"></script>
+    <script src="../src/components/footer.js"></script>
+    <title>Lista de Categorías</title>
+</head>
+<header-component></header-component>
+<body>
+    <?php 
+    require 'cabecera.php'; ?>
+    <h1>Lista de categorías</h1>
+    <!--lista de vínculos con la forma de productos.php?categoria=1-->
+
+    <?php
+    $categorias = cargar_categorias();
+    if ($categorias === FALSE) {
+        echo "<p class='error'>Error al conectar con la base de datos</p>";
+    } else {
+        echo "<ul>"; //abrir la lista
+        foreach ($categorias as $cat) {
+            //$cat['nombre] $cat['codCat']
+            $url = "productos.php?categoria=" . $cat['codCat'];
+            echo "<li class='lista'><a href='$url'>" . $cat['nombre'] . "</a></li>";
+        }
+        echo "</ul>";
+    }
+    ?>
+    <footer-component></footer-component>
+</body>
+</html>
---
+++ .history/login_20220218093446.php
@@ -0,0 +1,53 @@
+<?php
+require_once 'bd.php';
+// formulario de login habitual. Si va bien abre la sesión, guarda el nombre de usuario
+//  y redirige a principal.php. Si va mal, mensaje de error
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$usu = comprobar_usuario($_POST['usuario'], $_POST['clave']);
+if($usu===FALSE){
+    $err = TRUE;
+    $usuario = $_POST['usuario'];
+    }else{
+        session_start();
+        // $usu tiene campos correo y codRes, correo
+        $_SESSION['usuario'] = $usu;
+        $_SESSION['carrito'] = [];
+        header("Location: categorias.php");
+        return;
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="/src/components/header.js"></script>
+    <script src="/src/components/footer.js"></script>
+    <title>Formulario de login</title>
+    
+</head>
+<header-component></header-component>
+<body>
+<h1>Inicia a sesión</h1>
+
+<?php if (isset($_GET["redirigido"])){
+    echo "<p>Haga login para continuar</p>";
+} ?>
+<?php if (isset($err) and $err == TRUE){
+    echo "<p>Revise usuario y contraseña</p>";
+}?>
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+    <label for="usuario">Usuario</label>
+    <input value = "<?php if(isset($usuario))echo $usuario;?>"
+    id="usuario" name="usuario" type="text">
+    <label for = "clave">Clave</label>
+    <input id="clave" name="clave" type="password">
+    <input type="submit">
+</form>
+<footer-component></footer-component>
+</body>
+</html>
